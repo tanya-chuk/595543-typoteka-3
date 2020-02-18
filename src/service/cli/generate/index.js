12d3da8d2998a5a40f19bc05ae621acbbd41c9d9
Tanya Chuk <taosadchuk@gmail.com>
2020-02-18 17:01:30 +0300

    Генерирует моки с помощью async/await

--- src/service/cli/generate/index.js
+++ src/service/cli/generate/index.js
@@ -3,7 +3,7 @@
 const {DEFAULT_COUNT, MAX_SENTENCES, FILE_NAME, TITLES, SENTENCES, CATEGORIES} = require(`./constants`);
 const {getItems, getDate} = require(`./utils`);
 const {getRandomInt} = require(`../../utils`);
-const fs = require(`fs`);
+const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 
 const generateOffers = (count) => (
@@ -18,16 +18,16 @@ const generateOffers = (count) => (
 
 module.exports = {
   name: `--generate`,
-  run(args) {
+  async run(args) {
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     const content = JSON.stringify(generateOffers(countOffer));
 
-    fs.writeFile(FILE_NAME, content, (err) => {
-      if (err) {
-        return console.error(chalk.red(`Can't write data to file...`));
-      }
-      return console.info(chalk.green(`Operation success. File created.`));
-    });
+    try {
+      await fs.writeFile(FILE_NAME, content);
+      console.info(chalk.green(`Operation success. File created.`));
+    } catch (err) {
+      console.error(chalk.red(`Can't write data to file...`));
+    }
   }
 };
