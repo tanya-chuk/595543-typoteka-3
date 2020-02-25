c2805e517620234306693e541e4ee2700676fa9f
Tanya Chuk <taosadchuk@gmail.com>
2020-02-25 20:16:18 +0300

    Исправляет замечания после ревью

--- src/express/index.js
+++ src/express/index.js
@@ -1,23 +1,12 @@
 'use strict';
 
 const express = require(`express`);
-const DEFAULT_PORT = 8080;
-const myRouter = require(`./routes/my`);
-const offersRouter = require(`./routes/offers`);
-const searchRouter = require(`./routes/search`);
-const registerRouter = require(`./routes/register`);
-const loginRouter = require(`./routes/login`);
-const {RoutesNames} = require(`./routes/constants`);
+const appRouter = require(`./routes`);
 
-const {my, offers, search, register, login} = RoutesNames;
+const DEFAULT_PORT = 8080;
 const app = express();
 
-app.get(`/`, (req, res) => res.send(`/`));
-app.use(my, myRouter);
-app.use(offers, offersRouter);
-app.use(search, searchRouter);
-app.use(register, registerRouter);
-app.use(login, loginRouter);
+app.use(appRouter);
 
 app.listen(DEFAULT_PORT,
     () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
