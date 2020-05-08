aaedf7b23b643890a690d9723d22dc806f7dde38
Tanya Chuk <taosadchuk@gmail.com>
2020-05-08 17:35:39 +0300

    Добавляет шаблоны страниц

--- src/express/index.js
+++ src/express/index.js
@@ -1,10 +1,14 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
 const appRouter = require(`./routes`);
 
 const DEFAULT_PORT = 8080;
 const app = express();
+
+app.set(`views`, path.join(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(appRouter);
 
