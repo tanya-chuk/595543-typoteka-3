c46728c693260259fa645c24743b68149c84ae7f
Tanya Chuk <taosadchuk@gmail.com>
2020-05-08 20:19:03 +0300

    Исправляет путь к странице

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -8,7 +8,7 @@ const appRouter = new Router();
 
 appRouter.get(`/`, (req, res) => res.render(`pages/main`));
 appRouter.get(`/search`, (req, res) => res.render(`pages/search`));
-appRouter.get(`/register`, (req, res) => res.send(`/register`));
+appRouter.get(`/register`, (req, res) => res.render(`pages/sign-up`));
 appRouter.get(`/login`, (req, res) => res.render(`pages/login`));
 appRouter.get(`/categories`, (req, res) => res.render(`pages/all-categories`));
 appRouter.use(`/my`, myRouter);
