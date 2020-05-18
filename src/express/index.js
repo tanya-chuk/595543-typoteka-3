d7cb5980b6a88a19c4f63f715b41a81451391cb9
Tanya Chuk <taosadchuk@gmail.com>
2020-05-18 22:00:49 +0300

    Исправляет разметку

--- src/express/index.js
+++ src/express/index.js
@@ -7,10 +7,15 @@ const appRouter = require(`./routes`);
 const DEFAULT_PORT = 8080;
 const app = express();
 
-app.set(`views`, path.join(__dirname, `templates`));
+const getDirection = (pathName) => {
+  return path.join(__dirname, pathName);
+};
+
+app.set(`views`, getDirection(`templates`));
 app.set(`view engine`, `pug`);
 
 app.use(appRouter);
+app.use(express.static(getDirection(`../../markup`)));
 
 app.listen(DEFAULT_PORT,
     () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
